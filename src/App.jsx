dd1ebaaeadc5bfec12128ee93fd48092089846ba
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2025-03-17 21:36:38 +0530

    Added styling to the website

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,22 @@ function App(){
           <h4>For we understand your tastebuds</h4>
         </div>
       </div>
+      <h1 className={styles.our_menu} style={{color:"white",textAlign:"center",fontSize:"50px",marginBottom:"50px"}}>Our Menu</h1>
       <div className={styles.menu}>
-        <Menu name="Espress" desc="Experience the bold, rich flavor of our finely crafted espresso—small in size, but big in energy!"/>
+        <Menu name="Espresso" desc="Experience the bold, rich flavor of our finely crafted espresso—small in size, but big in energy!"/>
         <Menu name="Cappuccino" desc="A perfect balance of espresso, velvety steamed milk, and a cloud of foam—indulge in the classic Italian coffee experience!"/>
         <Menu name="Latte" desc="Smooth, creamy, and irresistibly. Latte blends espresso with steamed milk for a sip of pure bliss!"/>
         <Menu name="Mocha" desc="Chocolate meets coffee in a match made in heaven. Our mocha combines rich espresso, creamy milk, and luxurious cocoa"/>
-        <Menu name="Americano" desc="Bold espresso meets hot water for a smooth, robust flavor—perfect for those who love their coffee strong but easy to sip!"/>
         <Menu name="Macchiato" desc="A bold espresso with a touch of frothy milk—simple, elegant, and packed with flavor in every sip!"/>
         <Menu name="Flat White" desc="A velvety blend of espresso and microfoam—stronger than a latte, smoother than a cappuccino. The ultimate coffee lover's choice!"/>      
         <Menu name="Cold Brew" desc="Slow-steeped for 12+ hours, our cold brew delivers a smooth, naturally sweet coffee with low acidity and a refreshing kick."/>
-        <Menu name="Affogato" desc="A heavenly mix of hot espresso poured over creamy vanilla ice cream. Dessert and coffee—why choose when you can have both?"/>
         <Menu name="Turkish Coffee" desc="Rich, thick, and brewed to perfection in a traditional copper pot. Our Turkish coffee is a sip of history, with an unforgettable bold taste."/>
       </div>
       <div className={styles.canva}>
           <canvas className="canva"></canvas>
       </div>
       <div className={styles.footer}>
-          <div style={{textAlign:"center",fontSize:"50px"}}>Book your seat now!!! <button>Book</button></div><br></br><br></br><br></br>
+          <div style={{textAlign:"center",fontSize:"50px"}}>Book your seat now!!! <button className={styles.book}>Book</button></div><br></br><br></br><br></br>
           Contact no.: +91 9876543210<br></br><br></br><br></br>
           Email: abc123@gmail.com<br></br><br></br><br></br><br></br><br></br><br></br>
           <div style={{textAlign:"center"}}>Created by aysh_mzmdr</div>
--- src/menu.jsx
+++ src/menu.jsx
@@ -4,7 +4,7 @@ function menu(props){
 
     return(
         <div className={styles.box}>
-            <h3 style={{fontSize:"40px"}}>{props.name}</h3>
+            <h3 style={{fontSize:"35px"}}>{props.name}</h3>
             -------------------------------------
             <p style={{paddingInline:"10px",textAlign:"center",fontSize:"25px"}}>{props.desc}</p>
         </div>
